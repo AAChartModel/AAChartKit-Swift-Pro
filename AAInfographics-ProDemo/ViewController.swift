cbc47dec6f834f90119eddbd267fa07b875d1acd
AnAn <2236368544@qq.com>
2020-11-10 13:54:26 +0800

    Add project basic content

---
+++ AAInfographics-ProDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AAInfographics-ProDemo
+//
+//  Created by AnAn on 2020/11/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
