e383f7dbd697f8d09c5d1a61b12aa899da9a27d5
An An <2236368544@qq.com>
2022-05-26 15:43:50 +0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+     name: "AAInfographics-Pro",
+     platforms: [
+         .iOS(.v9),
+         .macOS(.v10_11)
+     ],
+     products: [
+         .library(name: "AAInfographics-Pro", targets: ["AAInfographics-Pro"])
+     ],
+     targets: [
+        .target(
+			name: "AAInfographics-Pro",
+			path: "AAInfographics-Pro",
+            exclude: ["Info.plist", "ProjectBundlePathLoader.swift"],
+			resources: [
+				.copy("AAJSFiles.bundle")
+			]
+        )
+     ]
+ )
