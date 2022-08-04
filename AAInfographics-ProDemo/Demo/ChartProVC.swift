8f35c35af2d2ddfe5715dbcc014fecbb5cff1d6f
An An <2236368544@qq.com>
2022-08-04 19:01:18 +0800

    Update ChartProVC.swift

--- AAInfographics-ProDemo/Demo/ChartProVC.swift
+++ AAInfographics-ProDemo/Demo/ChartProVC.swift
@@ -1402,70 +1402,66 @@ class ChartProVC: AABaseChartVC {
             .chart(AAChart()
                 .type(AAChartType.solidgauge)
 //                .height("110%")
-//                .events(AAChartEvents()
-//                    .render(#"""
-//                    function renderIcons() {
-//
-//                // Move icon
-//                if (!this.series[0].icon) {
-//                    this.series[0].icon = this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8])
-//                        .attr({
-//                            stroke: '#303030',
-//                            'stroke-linecap': 'round',
-//                            'stroke-linejoin': 'round',
-//                            'stroke-width': 2,
-//                            zIndex: 10
-//                        })
-//                        .add(this.series[2].group);
-//                }
-//                this.series[0].icon.translate(
-//                    this.chartWidth / 2 - 10,
-//                    this.plotHeight / 2 - this.series[0].points[0].shapeArgs.innerR -
-//                    (this.series[0].points[0].shapeArgs.r - this.series[0].points[0].shapeArgs.innerR) / 2
-//                );
-//
-//                // Exercise icon
-//                if (!this.series[1].icon) {
-//                    this.series[1].icon = this.renderer.path(
-//                        ['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8,
-//                            'M', 8, -8, 'L', 16, 0, 8, 8
-//                        ]
-//                    )
-//                        .attr({
-//                            stroke: '#ffffff',
-//                            'stroke-linecap': 'round',
-//                            'stroke-linejoin': 'round',
-//                            'stroke-width': 2,
-//                            zIndex: 10
-//                        })
-//                        .add(this.series[2].group);
-//                }
-//                this.series[1].icon.translate(
-//                    this.chartWidth / 2 - 10,
-//                    this.plotHeight / 2 - this.series[1].points[0].shapeArgs.innerR -
-//                    (this.series[1].points[0].shapeArgs.r - this.series[1].points[0].shapeArgs.innerR) / 2
-//                );
-//
-//                // Stand icon
-//                if (!this.series[2].icon) {
-//                    this.series[2].icon = this.renderer.path(['M', 0, 8, 'L', 0, -8, 'M', -8, 0, 'L', 0, -8, 8, 0])
-//                        .attr({
-//                            stroke: '#303030',
-//                            'stroke-linecap': 'round',
-//                            'stroke-linejoin': 'round',
-//                            'stroke-width': 2,
-//                            zIndex: 10
-//                        })
-//                        .add(this.series[2].group);
-//                }
-//
-//                this.series[2].icon.translate(
-//                    this.chartWidth / 2 - 10,
-//                    this.plotHeight / 2 - this.series[2].points[0].shapeArgs.innerR -
-//                    (this.series[2].points[0].shapeArgs.r - this.series[2].points[0].shapeArgs.innerR) / 2
-//                );
-//            }
-//                    """#))
+                .events(AAChartEvents()
+                    .load("""
+                    function () {
+                    if (!this.series[0].icon) {
+                    this.series[0].icon = this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8])
+                        .attr({
+                            stroke: '#303030',
+                            'stroke-linecap': 'round',
+                            'stroke-linejoin': 'round',
+                            'stroke-width': 2,
+                            zIndex: 10
+                        })
+                        .add(this.series[2].group);
+                    }
+                    this.series[0].icon.translate(
+                    this.chartWidth / 2 - 10,
+                    this.plotHeight / 2 - this.series[0].points[0].shapeArgs.innerR -
+                    (this.series[0].points[0].shapeArgs.r - this.series[0].points[0].shapeArgs.innerR) / 2
+                    );
+                    
+                            if (!this.series[1].icon) {
+                    this.series[1].icon = this.renderer.path(
+                        ['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8,
+                            'M', 8, -8, 'L', 16, 0, 8, 8
+                        ]
+                    )
+                        .attr({
+                            stroke: '#ffffff',
+                            'stroke-linecap': 'round',
+                            'stroke-linejoin': 'round',
+                            'stroke-width': 2,
+                            zIndex: 10
+                        })
+                        .add(this.series[2].group);
+                    }
+                    this.series[1].icon.translate(
+                    this.chartWidth / 2 - 10,
+                    this.plotHeight / 2 - this.series[1].points[0].shapeArgs.innerR -
+                    (this.series[1].points[0].shapeArgs.r - this.series[1].points[0].shapeArgs.innerR) / 2
+                    );
+                    
+                            if (!this.series[2].icon) {
+                    this.series[2].icon = this.renderer.path(['M', 0, 8, 'L', 0, -8, 'M', -8, 0, 'L', 0, -8, 8, 0])
+                        .attr({
+                            stroke: '#303030',
+                            'stroke-linecap': 'round',
+                            'stroke-linejoin': 'round',
+                            'stroke-width': 2,
+                            zIndex: 10
+                        })
+                        .add(this.series[2].group);
+                    }
+                    
+                    this.series[2].icon.translate(
+                    this.chartWidth / 2 - 10,
+                    this.plotHeight / 2 - this.series[2].points[0].shapeArgs.innerR -
+                    (this.series[2].points[0].shapeArgs.r - this.series[2].points[0].shapeArgs.innerR) / 2
+                    );
+                    }
+                    """))
             )
             .title(AATitle()
                 .text("Activity")
@@ -1481,14 +1477,14 @@ class ChartProVC: AABaseChartVC {
                 .valueSuffix("%")
 //                .pointFormat("{series.name}
 //        {point.y}")
-                .positioner(#"""
+                .positioner("""
                 function(labelWidth) {
                         return {
                             x: (this.chart.chartWidth - labelWidth) / 2 - 100,
                             y: (this.chart.plotHeight / 2) + 15
                         };
                     }
-                """#))
+                """))
             .pane(AAPane()
                 .startAngle(0)
                 .endAngle(360)
