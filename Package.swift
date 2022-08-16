5030b28f8c1ff41397123d618fef6f85fe7a7422
An An <2236368544@qq.com>
2022-08-16 14:39:27 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let package = Package(
      name: "AAInfographics-Pro",
      platforms: [
-         .iOS(.v9),
-         .macOS(.v10_11)
+         .iOS(.v10),
+         .macOS(.v10_13)
      ],
      products: [
          .library(name: "AAInfographics-Pro", targets: ["AAInfographics-Pro"])
