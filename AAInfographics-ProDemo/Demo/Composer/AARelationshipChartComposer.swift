95b6a1bd5db7713ace8fc8745d8b9a779b6560cc
An An <2236368544@qq.com>
2025-09-26 19:03:20 +0800

    Add custom node coloring to networkgraph chart
    Introduced a beforeDrawChartJavaScript event to dynamically assign colors and marker radii to nodes in the networkgraph chart, enhancing visual distinction between parent, child, and grandchild nodes.

--- AAInfographics-ProDemo/Demo/Composer/AARelationshipChartComposer.swift
+++ AAInfographics-ProDemo/Demo/Composer/AARelationshipChartComposer.swift
@@ -183,6 +183,57 @@ class AARelationshipChartComposer {
     
     static func networkgraphChart() -> AAOptions {
         AAOptions()
+            .beforeDrawChartJavaScript(
+                """
+                // Add the nodes option through an event call. We want to start with the parent
+                // item and apply separate colors to each child element, then the same color to
+                // grandchildren.
+                Highcharts.addEvent(
+                    Highcharts.Series,
+                    'afterSetOptions',
+                    function (e) {
+
+                        const colors = Highcharts.getOptions().colors,
+                            nodes = {};
+
+                        let i = 0;
+
+                        if (
+                            this instanceof Highcharts.Series.types.networkgraph &&
+                            e.options.id === 'lang-tree'
+                        ) {
+                            e.options.data.forEach(function (link) {
+
+                                if (link[0] === 'Proto Indo-European') {
+                                    nodes['Proto Indo-European'] = {
+                                        id: 'Proto Indo-European',
+                                        marker: {
+                                            radius: 20
+                                        }
+                                    };
+                                    nodes[link[1]] = {
+                                        id: link[1],
+                                        marker: {
+                                            radius: 10
+                                        },
+                                        color: colors[i++]
+                                    };
+                                } else if (nodes[link[0]] && nodes[link[0]].color) {
+                                    nodes[link[1]] = {
+                                        id: link[1],
+                                        color: nodes[link[0]].color
+                                    };
+                                }
+                            });
+
+                            e.options.nodes = Object.keys(nodes).map(function (id) {
+                                return nodes[id];
+                            });
+                        }
+                    }
+                );
+                """
+            )
             .chart(AAChart()
                 .type(.networkgraph))
             .title(AATitle()
